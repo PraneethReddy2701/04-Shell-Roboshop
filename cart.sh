cb703912e993d2fbc621269e02761ab7d0f51f3e
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-08 22:03:52 +0530

    cart component

--- cart.sh
+++ cart.sh
@@ -9,17 +9,17 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
-$SCRIPT_DIR=$PWD
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "Script started running at : $(date)"   &>>$LOG_FILE
+echo "Script started running at : $(date)"   | tee -a $LOG_FILE
 
-if [ $? -ne 0 ]
+if [ $USER_ID -ne 0 ]
 then
-    echo -e "$R ERROR $N: Please run the script with root user"  &>>$LOG_FILE
+    echo -e "$R ERROR $N: Please run the script with root user"  | tee -a $LOG_FILE
     exit 1
 else
-    echo "Running the script with root user"  &>>$LOG_FILE
+    echo "Running the script with root user"  | tee -a $LOG_FILE
 fi
 
 VALIDATE()
@@ -48,7 +48,7 @@ then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop    &>>$LOG_FILE
     VALIDATE $? "Creating the system user roboshop"
 else
-    echo -e "System user roboshop is already created.. $Y SO SKIPPING $N"   &>>$LOG_FILE
+    echo -e "System user roboshop is already created.. $Y SO SKIPPING $N"   | tee -a $LOG_FILE
 fi
 
 mkdir -p /app   &>>$LOG_FILE
